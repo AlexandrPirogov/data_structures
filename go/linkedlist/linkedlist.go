fac9f536666491cf201d08267be35141ae4ed214
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-08-16 14:32:40 +0300

    added linked list impl

---
+++ go/linkedlist/linkedlist.go
@@ -0,0 +1,177 @@
+package linkedlist
+
+import "errors"
+
+type Node struct {
+	next  *Node
+	value int
+}
+
+var deleteType map[bool]func(l *LinkedList, n int) = map[bool]func(l *LinkedList, n int){
+	true:  deleteAll,
+	false: deleteOne,
+}
+
+type LinkedList struct {
+	head *Node
+	tail *Node
+}
+
+func (l *LinkedList) AddInTail(item Node) {
+	if l.head == nil {
+		l.head = &item
+	} else {
+		l.tail.next = &item
+	}
+
+	l.tail = &item
+}
+
+func (l *LinkedList) Count() int {
+	tmp := l.head
+	count := 0
+
+	pred := func() bool { return tmp != nil }
+	act := func() {
+		count++
+		tmp = tmp.next
+	}
+
+	iter(pred, act)
+	return count
+}
+
+// error не nil, если узел не найден
+func (l *LinkedList) Find(n int) (Node, error) {
+	res := &Node{value: -1, next: nil}
+
+	tmp := l.head
+	found := false
+
+	pred := func() bool { return tmp != nil && !found }
+	act := func() {
+		if tmp.value == n {
+			res = tmp
+			found = true
+		}
+		tmp = tmp.next
+	}
+
+	iter(pred, act)
+
+	if !found {
+		return *res, errors.New("not found")
+	}
+	return *res, nil
+}
+
+func (l *LinkedList) FindAll(n int) []Node {
+	nodes := make([]Node, 0)
+	tmp := l.head
+
+	pred := func() bool { return tmp != nil }
+	act := func() {
+		if tmp.value == n {
+			nodes = append(nodes, *tmp)
+		}
+		tmp = tmp.next
+	}
+
+	iter(pred, act)
+	return nodes
+}
+
+func (l *LinkedList) Delete(n int, all bool) {
+	if l.head == nil {
+		return
+	}
+
+	deleteF := deleteType[all]
+	deleteF(l, n)
+}
+
+func (l *LinkedList) Insert(after *Node, add Node) {
+	tmp := l.head
+	pred := func() bool { return tmp.value != add.value }
+	act := func() { tmp = tmp.next }
+
+	iter(pred, act)
+
+	add.next = tmp.next
+	tmp.next = &add
+}
+
+func (l *LinkedList) InsertFirst(first Node) {
+	first.next = l.head
+	l.head = &first
+}
+
+func (l *LinkedList) Clean() {
+	l.head, l.tail = nil, nil
+}
+
+func deleteOne(l *LinkedList, n int) {
+
+	if l.head.value == n {
+		l.head = l.head.next
+		return
+	}
+
+	tmp := l.head
+	deleted := false
+
+	pred := func() bool { return !deleted && tmp.next != nil }
+	act := func() {
+		if tmp.next.value == n {
+			tmp.next = tmp.next.next
+			deleted = true
+		}
+		tmp = tmp.next
+	}
+
+	iter(pred, act)
+}
+
+func deleteAll(l *LinkedList, n int) {
+	deleteAllHead(l, n)
+	deleteAllBody(l, n)
+}
+
+func deleteAllHead(l *LinkedList, n int) {
+	pred := func() bool {
+		return l.head != nil && l.head.value == n
+	}
+
+	a := func() {
+		l.head = l.head.next
+	}
+
+	iter(pred, a)
+}
+
+func deleteAllBody(l *LinkedList, n int) {
+	tmp := l.head
+	pred := func() bool { return tmp != nil && tmp.next != nil }
+
+	act := func() {
+		if tmp.next.value == n {
+			tmp.next = tmp.next.next
+		}
+		tmp = tmp.next
+	}
+
+	iter(pred, act)
+}
+
+func iter(cond func() bool, action func()) {
+	for cond() {
+		action()
+	}
+}
+
+func cond(ift func() bool, act func(), continu func()) {
+	if ift() {
+		act()
+	}
+	continu()
+}
---
+++ go/linkedlist/linkedlist_test.go
@@ -0,0 +1,283 @@
+package linkedlist
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// assert that created new ll is empty
+func TestCreateList(t *testing.T) {
+	//arrange
+	sut := LinkedList{}
+
+	assert.Nil(t, sut.head)
+	assert.Nil(t, sut.head)
+	assert.Equal(t, sut.Count(), 0)
+}
+
+func TestAddInEmptyList(t *testing.T) {
+	sut := LinkedList{}
+
+	sut.AddInTail(Node{nil, 10})
+
+	assert.Equal(t, sut.Count(), 1)
+	assert.Equal(t, sut.head, sut.tail)
+
+	assert.NotNil(t, sut.head)
+}
+
+// Adding many different notes
+func TestAddManyList(t *testing.T) {
+	tests := []Node{}
+
+	for i := 0; i < 10; i++ {
+		tests = append(tests, Node{nil, i})
+	}
+
+	sut := LinkedList{}
+
+	for _, edge := range tests {
+		t.Run(fmt.Sprintf("%d", edge.value), func(t *testing.T) {
+			sut.AddInTail(edge)
+			assert.Equal(t, sut.tail, &edge)
+		})
+	}
+}
+
+// checks find method with list
+// with distinct values
+func TestFindInSetList(t *testing.T) {
+	sut := LinkedList{}
+	for i := 0; i < 10; i++ {
+		sut.AddInTail(Node{nil, i})
+	}
+
+	for i := 0; i < 10; i++ {
+		t.Run("", func(t *testing.T) {
+			res, err := sut.Find(i)
+			assert.Nil(t, err)
+			assert.Equal(t, i, res.value)
+		})
+	}
+}
+
+// Checks find method in empty list
+func TestFindInEmptyList(t *testing.T) {
+	sut := LinkedList{}
+	res, err := sut.Find(10)
+	assert.NotNil(t, err)
+	assert.Equal(t, -1, res.value)
+}
+
+// Checks find method in list with duplicates
+func TestFindInList(t *testing.T) {
+	duplicate := 10
+	sut := LinkedList{}
+	for i := 0; i < 10; i++ {
+		sut.AddInTail(Node{nil, duplicate})
+	}
+
+	for i := 0; i < 10; i++ {
+		t.Run("", func(t *testing.T) {
+			res, err := sut.Find(duplicate)
+			assert.Nil(t, err)
+			assert.Equal(t, duplicate, res.value)
+		})
+	}
+}
+
+// Checks that findall returns empty slice for empty list
+func TestFindAllInEmptyList(t *testing.T) {
+	sut := LinkedList{}
+
+	res := sut.FindAll(10)
+	assert.Equal(t, len(res), 0)
+
+}
+
+// checks that findall in set list returns exactly slice with single elem
+func TestFindAllInSetList(t *testing.T) {
+	sut := LinkedList{}
+	for i := 0; i < 10; i++ {
+		sut.AddInTail(Node{nil, i})
+	}
+
+	for i := 0; i < 10; i++ {
+		t.Run("", func(t *testing.T) {
+			res := sut.FindAll(i)
+			assert.Equal(t, len(res), 1)
+		})
+	}
+}
+
+// checks that findall in list with duplicates
+// returns slice with all elems in lsit
+func TestFindAllInList(t *testing.T) {
+	duplicate := 10
+	sut := LinkedList{}
+	for i := 0; i < 10; i++ {
+		sut.AddInTail(Node{nil, duplicate})
+	}
+
+	for i := 0; i < 10; i++ {
+		t.Run("", func(t *testing.T) {
+			res := sut.FindAll(duplicate)
+			assert.Equal(t, len(res), sut.Count())
+		})
+	}
+}
+
+func TestCleanEmptyList(t *testing.T) {
+	sut := LinkedList{}
+
+	sut.Clean()
+
+	assert.Nil(t, sut.head)
+	assert.Nil(t, sut.tail)
+
+	assert.Equal(t, 0, sut.Count())
+}
+
+func TestCleanList(t *testing.T) {
+	sut := LinkedList{}
+	for i := 0; i < 10; i++ {
+		sut.AddInTail(Node{nil, i})
+	}
+
+	sut.Clean()
+
+	assert.Nil(t, sut.head)
+	assert.Nil(t, sut.tail)
+
+	assert.Equal(t, 0, sut.Count())
+}
+
+// checks that insert first make head equal inserted elem
+func TestInsertFirstInEmptyList(t *testing.T) {
+	sut := LinkedList{}
+
+	insert := Node{nil, 1}
+	sut.InsertFirst(insert)
+
+	assert.Equal(t, sut.head, &insert)
+	assert.Nil(t, sut.head.next)
+}
+
+func TestInsertFirstInList(t *testing.T) {
+	sut := LinkedList{}
+	for i := 0; i < 10; i++ {
+		sut.AddInTail(Node{nil, i})
+	}
+
+	insert := Node{nil, 20}
+	sut.InsertFirst(insert)
+
+	assert.Equal(t, sut.head.value, insert.value)
+}
+
+func TestInsertInList(t *testing.T) {
+	sut := LinkedList{}
+	nodes := make([]Node, 0)
+	for i := 0; i < 10; i++ {
+		add := Node{nil, i}
+		nodes = append(nodes, add)
+		sut.AddInTail(add)
+	}
+
+	for k, edge := range nodes {
+		t.Run("", func(t *testing.T) {
+			sut.Insert(&edge, Node{nil, k})
+			res := sut.FindAll(k)
+
+			assert.Equal(t, len(res), 2)
+		})
+	}
+
+	for _, edge := range nodes {
+		t.Run("", func(t *testing.T) {
+			node, err := sut.Find(edge.value)
+
+			assert.Nil(t, err)
+			assert.Equal(t, node.next.value, edge.value)
+		})
+	}
+}
+
+func TestDeleteFromEmptyList(t *testing.T) {
+	sut := LinkedList{}
+	sut.Delete(10, false)
+
+	assert.Nil(t, sut.head)
+	assert.Equal(t, 0, sut.Count())
+}
+
+func TestDeleteAllFromEmptyList(t *testing.T) {
+	sut := LinkedList{}
+	sut.Delete(10, true)
+
+	assert.Nil(t, sut.head)
+	assert.Equal(t, 0, sut.Count())
+}
+
+func TestDeleteOneFromList(t *testing.T) {
+	sut := LinkedList{}
+	nodes := make([]Node, 0)
+	for i := 0; i < 10; i++ {
+		add := Node{nil, i}
+		nodes = append(nodes, add)
+		sut.AddInTail(add)
+	}
+	for _, edge := range nodes {
+		t.Run("", func(t *testing.T) {
+			oldcount := sut.Count()
+			sut.Delete(edge.value, false)
+			newcount := sut.Count()
+
+			assert.Greater(t, oldcount, newcount)
+		})
+	}
+}
+
+func TestDeleteAllFromList(t *testing.T) {
+	sut := LinkedList{}
+	nodes := []Node{
+		{
+			nil,
+			1,
+		},
+		{
+			nil,
+			1,
+		},
+		{
+			nil,
+			2,
+		},
+		{
+			nil,
+			3,
+		},
+		{
+			nil,
+			3,
+		},
+		{
+			nil,
+			2,
+		},
+	}
+
+	for _, node := range nodes {
+		sut.AddInTail(node)
+	}
+
+	for _, edge := range nodes {
+		t.Run("", func(t *testing.T) {
+			sut.Delete(edge.value, true)
+		})
+	}
+
+	assert.Equal(t, 0, sut.Count())
+}
