75109b93f1eb33180f6ea8b5c06555f378794e40
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-08-17 13:48:50 +0300

    added doublelinkedlist

--- go/doublelinkedlist/main.go
+++ go/doublelinkedlist/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "errors"
+import (
+	"errors"
+)
 
 type Node struct {
 	prev  *Node
@@ -11,6 +13,11 @@ type Node struct {
 type LinkedList2 struct {
 	head *Node
 	tail *Node
+}
+
+var deleteF map[bool]func(int, *LinkedList2) = map[bool]func(int, *LinkedList2){
+	true:  deleteAll,
+	false: deleteOne,
 }
 
 func (l *LinkedList2) AddInTail(item Node) {
@@ -29,11 +36,15 @@ func (l *LinkedList2) AddInTail(item Node) {
 
 func (l *LinkedList2) Count() int {
 	tmp := l.head
+
 	count := 0
-	for tmp != nil {
+	pred := func() bool { return tmp != nil }
+	act := func() {
 		count++
 		tmp = tmp.next
 	}
+
+	iter(pred, act)
 	return count
 }
 
@@ -41,9 +52,10 @@ func (l *LinkedList2) Count() int {
 func (l *LinkedList2) Find(n int) (Node, error) {
 	res := Node{value: -1, next: nil}
 	found := false
-
 	tmp := l.head
-	for !found && tmp != nil {
+
+	pred := func() bool { return !found && tmp != nil }
+	act := func() {
 		if tmp.value == n {
 			res = *tmp
 			found = true
@@ -51,6 +63,7 @@ func (l *LinkedList2) Find(n int) (Node, error) {
 		tmp = tmp.next
 	}
 
+	iter(pred, act)
 	if !found {
 		return res, errors.New("not found")
 	}
@@ -61,27 +74,56 @@ func (l *LinkedList2) Find(n int) (Node, error) {
 func (l *LinkedList2) FindAll(n int) []Node {
 	nodes := make([]Node, 0)
 	tmp := l.head
-
-	for tmp != nil {
+	pred := func() bool { return tmp != nil }
+	act := func() {
 		if tmp.value == n {
 			nodes = append(nodes, *tmp)
 		}
 		tmp = tmp.next
 	}
 
+	iter(pred, act)
 	return nodes
 }
 
 func (l *LinkedList2) Delete(n int, all bool) {
+	if l.head == nil {
+		return
+	}
 
+	f := deleteF[all]
+	f(n, l)
 }
 
 func (l *LinkedList2) Insert(after *Node, add Node) {
+	if after == l.tail {
+		add.prev = after
+		after.next = &add
+		l.tail = &add
+		return
+	}
 
+	tmp := l.head
+	for tmp.value != after.value {
+		tmp = tmp.next
+	}
+
+	add.next = tmp.next
+	tmp.next.prev = &add
+	tmp.next = &add
+	add.prev = tmp
 }
 
 func (l *LinkedList2) InsertFirst(first Node) {
+	if l.head == nil {
+		l.head = &first
+		l.tail = &first
+		return
+	}
 
+	l.head.prev = &first
+	first.next = l.head
+	l.head = &first
 }
 
 func (l *LinkedList2) Clean() {
@@ -89,6 +131,100 @@ func (l *LinkedList2) Clean() {
 	l.tail = nil
 }
 
-func iter(pred func() bool, act func()) {
+// pretty bad code
+func deleteOne(n int, l *LinkedList2) {
+	if l.head == nil {
+		return
+	}
 
+	if l.head.value == n && l.head == l.tail {
+		l.head = nil
+		l.tail = nil
+		return
+	}
+
+	if l.head.value == n {
+		l.head = l.head.next
+		l.head.prev = nil
+		return
+	}
+
+	tmp := l.head
+
+	// pretty bad, but i still can't find out
+	// how to make it without nested if's
+	for tmp != nil {
+		if tmp.value == n {
+			if tmp == l.tail {
+				l.tail = l.tail.prev
+				l.tail.next = nil
+				return
+			} else {
+				tmp.next.prev = tmp.prev
+				tmp.prev.next = tmp.next
+				return
+			}
+		}
+		tmp = tmp.next
+	}
+
+}
+
+func deleteAll(n int, l *LinkedList2) {
+	deleteAllHead(n, l)
+	deleteAllTail(n, l)
+	deleteAllBody(n, l)
+}
+
+func deleteAllBody(n int, l *LinkedList2) {
+
+	tmp := l.head
+	pred := func() bool { return tmp != nil }
+	act := func() {
+		if tmp.value == n {
+			tmp.next.prev = tmp.prev
+			tmp.prev.next = tmp.next
+		}
+		tmp = tmp.next
+	}
+
+	iter(pred, act)
+}
+
+func deleteAllHead(n int, l *LinkedList2) {
+	pred := func() bool {
+		return l.head != l.tail && l.head.value == n
+	}
+
+	act := func() {
+		l.head = l.head.next
+		if l.head.prev != nil {
+			l.head.prev = nil
+		}
+	}
+
+	iter(pred, act)
+
+	if l.head == l.tail && l.head.value == n {
+		l.head, l.tail = nil, nil
+		return
+	}
+}
+
+func deleteAllTail(n int, l *LinkedList2) {
+	pred := func() bool { return l.tail != nil && l.tail.value == n }
+	act := func() {
+		l.tail = l.tail.prev
+		if l.tail.next != nil {
+			l.tail.next = nil
+		}
+	}
+
+	iter(pred, act)
+}
+
+func iter(pred func() bool, act func()) {
+	for pred() {
+		act()
+	}
 }
