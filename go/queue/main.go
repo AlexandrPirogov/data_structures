d68e0a7919a29a7b0a5327c26a7f8aa848002932
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-08-18 12:30:26 +0300

    added queue

---
+++ go/queue/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+//  "fmt" включите если используете
+
+type Queue[T any] struct {
+	count int
+	elems []T
+}
+
+func (q *Queue[T]) Size() int {
+	q = ifNil(q)
+	return q.count
+}
+
+func (q *Queue[T]) Dequeue() (T, error) {
+	q = ifNil(q)
+	var result T
+
+	if q.count == 0 {
+		return result, fmt.Errorf("queue is empty")
+	}
+
+	result = q.elems[0]
+	q.elems = q.elems[1:]
+	q.count--
+	return result, nil
+}
+
+func (q *Queue[T]) Enqueue(itm T) {
+	q = ifNil(q)
+	q.elems = append(q.elems, itm)
+	q.count++
+}
+
+func ifNil[T any](q *Queue[T]) *Queue[T] {
+	if q == nil {
+		return &Queue[T]{
+			count: 0,
+			elems: make([]T, 0),
+		}
+	}
+
+	return q
+}
---
+++ go/queue/main_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreate(t *testing.T) {
+	sut := Queue[int]{}
+
+	assert.Equal(t, 0, sut.Size())
+}
+
+func TestCountInNil(t *testing.T) {
+	var sut *Queue[int]
+
+	assert.Equal(t, 0, sut.Size())
+}
+
+func TestEnqueueInEmpty(t *testing.T) {
+	sut := Queue[int]{}
+
+	item := 1
+	sut.Enqueue(item)
+
+	assert.Equal(t, item, sut.count)
+}
+
+func TestDequeueFromEmpty(t *testing.T) {
+	var sut *Queue[int]
+
+	_, err := sut.Dequeue()
+	assert.NotNil(t, err)
+}
+
+func TestDequeueEnqueue(t *testing.T) {
+	sut := Queue[int]{}
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	for _, num := range nums {
+		sut.Enqueue(num)
+	}
+
+	for _, test := range nums {
+		t.Run("", func(t *testing.T) {
+			old := sut.count
+			res, err := sut.Dequeue()
+			new := sut.count
+
+			assert.Nil(t, err)
+			assert.Equal(t, old-1, new)
+			assert.Equal(t, test, res)
+		})
+	}
+
+	_, res := sut.Dequeue()
+	assert.NotNil(t, res)
+}
