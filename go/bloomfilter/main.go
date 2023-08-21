2616edf8ec8989d10bb577e14af4571358747517
AlexandrPirogov <devalexandrpirogov@gmail.com>
2023-08-21 12:25:32 +0300

    added bloom

---
+++ go/bloomfilter/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"os"
+)
+
+// битовый массив длиной f_len ...
+type BloomFilter struct {
+	filter_len int
+	bits       int
+}
+
+// хэш-функции
+// 17
+func (b *BloomFilter) Hash1(s string) int {
+	sum := 0
+	prev := 0
+	for _, char := range s {
+		code := int(char)
+		iter := ((code + prev*17) % b.filter_len)
+		prev = iter
+		sum = iter
+	}
+	// реализация ...
+	return sum
+}
+
+// 223
+func (b *BloomFilter) Hash2(s string) int {
+	sum := 0
+	prev := 0
+	for _, char := range s {
+		code := int(char)
+		iter := ((code + prev*223) % b.filter_len)
+		prev = iter
+		sum = iter
+	}
+	// реализация ...
+	return sum
+}
+
+// добавляем строку s в фильтр
+func (b *BloomFilter) Add(s string) {
+	n1, n2 := b.Hash1(s), b.Hash2(s)
+	b.bits = (b.bits | n1) | (b.bits | n2)
+}
+
+// проверка, имеется ли строка s в фильтре
+func (b *BloomFilter) IsValue(s string) bool {
+	n1, n2 := b.Hash1(s), b.Hash2(s)
+	return b.bits == (b.bits&n1)&(b.bits&n2)
+}
---
+++ go/bloomfilter/main_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var strings []string = []string{
+	"0123456789",
+	"1234567890",
+	"2345678901",
+	"3456789012",
+	"4567890123",
+	"5678901234",
+	"6789012345",
+	"7890123456",
+	"8901234567",
+	"9012345678",
+}
+
+func TestAddInEmpty(t *testing.T) {
+	sut := BloomFilter{32, 32}
+
+	for _, v := range strings {
+		t.Run(v, func(t *testing.T) {
+			assert.False(t, sut.IsValue(v))
+		})
+	}
+}
+
+func TestAddWithSingle(t *testing.T) {
+	sut := BloomFilter{32, 32}
+
+	sut.Add(strings[0])
+
+	for i := 1; i < len(strings); i++ {
+		t.Run(strings[i], func(t *testing.T) {
+			assert.False(t, sut.IsValue(strings[i]))
+		})
+	}
+}
+
+func TestAddPrettyEmpty(t *testing.T) {
+	sut := BloomFilter{32, 32}
+
+	for i := 0; i < len(strings)-1; i++ {
+		sut.Add(strings[0])
+	}
+
+	assert.False(t, sut.IsValue(strings[9]))
+}
