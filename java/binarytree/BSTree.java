576597fe4108b23f054bea88e4419ea7fb36d679
edual <edual@debian.debian>
2023-08-23 14:57:28 -0400

    added binary tree

---
+++ java/binarytree/BSTree.java
@@ -0,0 +1,187 @@
+import java.io.*;
+import java.util.*;
+
+class BSTNode<T> {
+  public int NodeKey; // ключ узла
+  public T NodeValue; // значение в узле
+  public BSTNode<T> Parent; // родитель или null для корня
+  public BSTNode<T> LeftChild; // левый потомок
+  public BSTNode<T> RightChild; // правый потомок
+
+  public BSTNode(int key, T val, BSTNode<T> parent) {
+    NodeKey = key;
+    NodeValue = val;
+    Parent = parent;
+    LeftChild = null;
+    RightChild = null;
+  }
+}
+
+// промежуточный результат поиска
+class BSTFind<T> {
+  // null если в дереве вообще нету узлов
+  public BSTNode<T> Node;
+
+  // true если узел найден
+  public boolean NodeHasKey;
+
+  // true, если родительскому узлу надо добавить новый левым
+  public boolean ToLeft;
+
+  public BSTFind() {
+    Node = null;
+  }
+}
+
+class BST<T> {
+  BSTNode<T> Root; // корень дерева, или null
+
+  public BST(BSTNode<T> node) {
+    Root = node;
+  }
+
+  public BSTFind<T> FindNodeByKey(int key) {
+    BSTFind<T> res = new BSTFind<T>();
+    if (Root == null) {
+      res.Node = null;
+      res.NodeHasKey = false;
+      return res;
+    }
+
+    BSTNode<T> curr = Root;
+    BSTNode<T> tmp = curr;
+    while (curr != null && curr.NodeKey != key) {
+      tmp = curr;
+      curr = curr.NodeKey > key ? curr.LeftChild : curr.RightChild;
+    }
+
+    if (curr == null) {
+      res.Node = tmp;
+      res.NodeHasKey = false;
+      res.ToLeft = (key < tmp.NodeKey) ? true : false;
+    } else {
+      res.Node = curr;
+      res.NodeHasKey = true;
+    }
+
+    return res;
+  }
+
+  public boolean AddKeyValue(int key, T val) {
+    if (Root == null) {
+      Root = new BSTNode<T>(key, val, null);
+      return true;
+    }
+
+    BSTFind<T> find = FindNodeByKey(key);
+    if (find.NodeHasKey) {
+      return false;
+    }
+
+    BSTNode<T> add = new BSTNode<T>(key, val, find.Node);
+    if (find.ToLeft) {
+      find.Node.LeftChild = add;
+      add.Parent = find.Node;
+    } else {
+      find.Node.RightChild = add;
+      add.Parent = find.Node;
+    }
+
+    // добавляем ключ-значение в дерево
+    return true; // если ключ уже есть
+  }
+
+  public BSTNode<T> FinMinMax(BSTNode<T> FromNode, boolean FindMax) {
+    return Root == null ? null :  (FindMax ? max(FromNode) : min(FromNode));
+  }
+
+  public boolean DeleteNodeByKey(int key) {
+    if (Root == null) {
+      Root = null;
+      return false;
+    }
+
+    if (Root.NodeKey == key) {
+      Root = null;
+      return true;
+    }
+
+    BSTFind<T> find = FindNodeByKey(key);
+    if (!find.NodeHasKey) {
+      return false;
+    }
+
+    BSTNode<T> curr = find.Node;
+    if (curr.LeftChild == null && curr.RightChild == null) {
+      curr = null;
+      return true;
+    }
+
+    if (curr.LeftChild != null && curr.RightChild == null) {
+      curr = curr.Parent;
+      curr.LeftChild = curr.LeftChild.LeftChild;
+      return true;
+    }
+
+    BSTNode<T> toReplace = FinMinMax(curr.RightChild, false);
+    curr.NodeKey = toReplace.NodeKey;
+    curr.NodeValue = toReplace.NodeValue;
+
+    BSTNode<T> tmp = curr.RightChild;
+    if (tmp.LeftChild == null) {
+      curr.RightChild = curr.RightChild.RightChild;
+      return true;
+    }
+
+    while (tmp.LeftChild.LeftChild != null) {
+      tmp = tmp.LeftChild;
+    }
+
+    tmp.LeftChild = null;
+    return true;
+
+  }
+
+  public int Count() {
+    if (Root == null) {
+      return 0;
+    }
+
+    int count = 0;
+    Stack<BSTNode<T>> stack = new Stack<BSTNode<T>>();
+    stack.add(Root);
+
+    while (!stack.empty()) {
+      BSTNode<T> curr = stack.pop();
+      count++;
+
+      if (curr.LeftChild != null) {
+        stack.add(curr.LeftChild);
+      }
+
+      if (curr.RightChild != null) {
+        stack.add(curr.RightChild);
+      }
+    }
+
+    return count; // количество узлов в дереве
+  }
+
+  private BSTNode<T> min(BSTNode<T> FromNode) {
+    BSTNode<T> tmp = FromNode;
+    while (tmp.LeftChild != null) {
+      tmp = tmp.LeftChild;
+    }
+
+    return tmp;
+  }
+
+  private BSTNode<T> max(BSTNode<T> FromNode) {
+    BSTNode<T> tmp = FromNode;
+    while (tmp.RightChild != null) {
+      tmp = tmp.RightChild;
+    }
+
+    return tmp;
+  }
+}
---
+++ java/binarytree/BSTreeTests.java
@@ -0,0 +1,264 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BSTreeTests {
+    
+    @Test
+    public void TestCreate() {
+        BST<Integer> sut = new BST<Integer>(null);
+        //assert that count == 0 and root is null
+
+        assertEquals(0, sut.Count());
+    }
+
+    @Test
+    public void TestAddKeyInEmpty() {
+        BSTNode<Integer> root = new BSTNode(0, 0, null);
+        BST<Integer> sut = new BST<Integer>(root);
+                
+        //assert that root == added key and count == 1
+        assertEquals(1, sut.Count());
+        assertEquals(root, sut.Root);
+    }
+
+    @Test
+    public void TestAddOnlyLeftKeys() {
+        int k = 10;
+        int v = 10;
+
+        BSTNode<Integer> root = new BSTNode(k, v, null);
+        BST<Integer> sut = new BST<Integer>(root);
+
+        for (int i = k; i > 0; i--) {
+            sut.AddKeyValue(k, v);
+            k--;
+            v--;
+        }
+        
+        //assert that root-> right is null and count == bunch of nodes
+        assertNull(sut.Root.RightChild);
+        assertEquals(10, sut.Count());
+    }
+
+    @Test
+    public void TestAddOnlyRightKeys() {
+        int k = 10;
+        int v = 10;
+
+        BSTNode<Integer> root = new BSTNode(k, v, null);
+        BST<Integer> sut = new BST<Integer>(root);
+
+        for (int i = k; i > 0; i--) {
+            sut.AddKeyValue(k, v);
+            k++;
+            v++;
+        }
+        
+        //assert that root-> right is null and count == bunch of nodes
+        assertNull(sut.Root.LeftChild);
+        assertEquals(10, sut.Count());
+    }
+
+    @Test
+    public void AddAllEqualsNodes() {
+        int k = 0;
+        int v = 0;
+
+        BSTNode<Integer> root = new BSTNode(k, v, null);
+        BST<Integer> sut = new BST<Integer>(root);
+        for (int i = 0; i < 10; i++) {
+            sut.AddKeyValue(k, v);
+        }
+
+        assertNull(sut.Root.LeftChild);
+        assertNull(sut.Root.RightChild);
+        assertEquals(1, sut.Count());
+    }
+
+    @Test
+    public void TestFindMinRoot() {
+        BSTNode<Integer> root = new BSTNode(0, 0, null);
+        BST<Integer> sut = new BST<Integer>(root);
+
+        for (int i = -5; i < 6; i++) {
+            sut.AddKeyValue(i, i);
+        }
+
+        BSTNode<Integer> min = sut.FinMinMax(root, false);
+        assertEquals(-5, min.NodeKey);
+    }
+
+
+      @Test
+    public void TestFindMax() {
+        BSTNode<Integer> root = new BSTNode(0, 0, null);
+        BST<Integer> sut = new BST<Integer>(root);
+
+        for (int i = -5; i < 6; i++) {
+            sut.AddKeyValue(i, i);
+        }
+
+        BSTNode<Integer> min = sut.FinMinMax(root, true);
+        assertEquals(5, min.NodeKey);
+    }
+
+     @Test
+    public void TestFindMinEmpty() {
+        BST<Integer> sut = new BST<Integer>(null);
+        BSTNode<Integer> res = sut.FinMinMax(sut.Root, false);
+        assertEquals(null, res);
+    }
+
+      @Test
+    public void TestFindMaxEmpty() {
+         BST<Integer> sut = new BST<Integer>(null);
+        BSTNode<Integer> res = sut.FinMinMax(sut.Root, true);
+        assertEquals(null, res);
+    }
+
+    @Test
+    public void TestFindValInEmpty() {
+        BST<Integer> sut = new BST<Integer>(null);
+        BSTFind<Integer> res = sut.FindNodeByKey(-1);
+
+        assertNull(res.Node);
+        assertFalse(res.NodeHasKey);
+        //assert that we can't find val in empty tree
+    }
+
+
+    @Test
+    public void TestFindExistingVal() {
+
+         BSTNode<Integer> root = new BSTNode(0, 0, null);
+        BST<Integer> sut = new BST<Integer>(root);
+
+        for (int i = -5; i < 6; i++) {
+            sut.AddKeyValue(i, i);
+        }
+
+        for (int i = -5; i < 6; i++) {
+            BSTFind<Integer> res = sut.FindNodeByKey(i);
+            assertTrue(res.NodeHasKey);
+        }
+
+    }
+
+    @Test
+    public void TestFindNotExistingVal() {
+        BSTNode<Integer> root = new BSTNode(0, 0, null);
+        BST<Integer> sut = new BST<Integer>(root);
+
+        for (int i = -5; i < 6; i++) {
+            sut.AddKeyValue(i, i);
+        }
+
+        for (int i = 20; i < 30; i++) {
+            BSTFind<Integer> res = sut.FindNodeByKey(i);
+            assertFalse(res.NodeHasKey);
+        }
+    }
+
+    @Test
+    public void TestDeleteFromEmpty() {
+        BST<Integer> sut = new BST<Integer>(null);
+        
+        int before = sut.Count();
+        boolean res = sut.DeleteNodeByKey(0);
+        int after = sut.Count();
+
+        assertFalse(res);
+        assertEquals(before, after);
+    }
+
+    @Test
+    public void TestDeleteRootSingle() {
+        BSTNode<Integer> root = new BSTNode<Integer>(0, 0, null);
+        BST<Integer> sut = new BST<Integer>(root);
+        
+        int before = sut.Count();
+        boolean res = sut.DeleteNodeByKey(0);
+        int after = sut.Count();
+
+        assertTrue(res);
+        assertNull(sut.Root);
+        assertEquals(before-1, after);
+    }
+
+    @Test
+    public void TestDeleteSubtreeWithoutRight() {
+        BSTNode<Integer> root = new BSTNode<Integer>(0, 0, null);
+        BST<Integer> sut = new BST<Integer>(root);
+        
+        sut.AddKeyValue(-1, -1);
+        sut.AddKeyValue(-2, -2);
+        sut.AddKeyValue(-3, -3);
+        
+
+        int before = sut.Count();
+        boolean res = sut.DeleteNodeByKey(-2);
+        int after = sut.Count();
+        BSTFind<Integer> find = sut.FindNodeByKey(-2);
+
+        assertTrue(res);
+        assertFalse(find.NodeHasKey);
+        assertEquals(before-1, after);
+    }
+
+    @Test
+    public void TestDeleteSubtreeWithRightWithLeft() {
+        BST<Integer> sut = new BST<Integer>(null);
+        
+        sut.AddKeyValue(8, 8);
+        sut.AddKeyValue(4, 4);
+        sut.AddKeyValue(6, 6);
+        sut.AddKeyValue(5, 5);
+        
+
+        int before = sut.Count();
+        boolean res = sut.DeleteNodeByKey(4);
+        int after = sut.Count();
+
+        assertTrue(res);
+        assertEquals(before-1, after);
+    }
+
+     @Test
+    public void TestDeleteSubtreeWithRightWithoutLeft() {
+        BSTNode<Integer> root = new BSTNode<Integer>(0, 0, null);
+        BST<Integer> sut = new BST<Integer>(root);
+        
+        sut.AddKeyValue(8, 8);
+        sut.AddKeyValue(4, 4);
+        sut.AddKeyValue(6, 6);
+        sut.AddKeyValue(7, 7);
+        
+
+        int before = sut.Count();
+        boolean res = sut.DeleteNodeByKey(4);
+        int after = sut.Count();
+
+        assertTrue(res);
+        assertEquals(before-1, after);
+    }
+
+    @Test
+    public void TestDeleteSubtreeWithRightWithLeft2() {
+        BSTNode<Integer> root = new BSTNode<Integer>(0, 0, null);
+        BST<Integer> sut = new BST<Integer>(root);
+        
+        sut.AddKeyValue(8, 8);
+        sut.AddKeyValue(4, 4);
+        sut.AddKeyValue(6, 6);
+        sut.AddKeyValue(7, 7);
+        
+
+        int before = sut.Count();
+        boolean res = sut.DeleteNodeByKey(4);
+        int after = sut.Count();
+
+        assertTrue(res);
+        assertEquals(before-1, after);
+    }
+}
