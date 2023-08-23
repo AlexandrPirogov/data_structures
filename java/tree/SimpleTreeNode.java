18097dccdcebf155dc611fc0d075755278ac294c
edual <edual@debian.debian>
2023-08-23 04:56:40 -0400

    java/
    added tree java implementation

---
+++ java/tree/SimpleTreeNode.java
@@ -0,0 +1,225 @@
+//import java.util.*;
+
+public class SimpleTreeNode<T> {
+  public T NodeValue; // значение в узле
+  public SimpleTreeNode<T> Parent; // родитель или null для корня
+  public List<SimpleTreeNode<T>> Children; // список дочерних узлов или null
+
+  public SimpleTreeNode(T val, SimpleTreeNode<T> parent) {
+    NodeValue = val;
+    Parent = parent;
+    Children = null;
+  }
+
+  public void AddChild(SimpleTreeNode<T> child) {
+    if (Children == null) {
+      Children = new ArrayList<SimpleTreeNode<T>>();
+    }
+    child.Parent = this;
+    Children.add(child);
+  }
+}
+
+class SimpleTree<T> {
+  public SimpleTreeNode<T> Root; // корень, может быть null
+
+  public SimpleTree(SimpleTreeNode<T> root) {
+    Root = root;
+  }
+
+  public void AddChild(SimpleTreeNode<T> ParentNode, SimpleTreeNode<T> NewChild) {
+    // ваш код добавления нового дочернего узла существующему ParentNode
+    if (Root == null) {
+      Root = NewChild;
+      Root.Children = new ArrayList<SimpleTreeNode<T>>();
+      return;
+    }
+
+    if (ParentNode == null) {
+      return;
+    }
+
+    Optional<SimpleTreeNode<T>> res = recSearch(ParentNode);
+    res.ifPresent(acc -> acc.AddChild(NewChild));
+  }
+
+  public void DeleteNode(SimpleTreeNode<T> NodeToDelete) {
+    // ваш код удаления существующего узла NodeToDelete
+    if (Root == null) {
+      return;
+    }
+
+    if (Root.equals(NodeToDelete)) {
+      Root = null;
+      return;
+    }
+
+    Stack<SimpleTreeNode<T>> stack = new Stack<SimpleTreeNode<T>>();
+    stack.add(Root);
+
+    while (!stack.empty()) {
+      SimpleTreeNode<T> Node = stack.pop();
+
+      if (Node.equals(NodeToDelete)) {
+        Node.Parent.Children.remove(Node);
+        Node = null;
+        return;
+      }
+
+      if (Node.Children == null) {
+        continue;
+      }
+
+      for (SimpleTreeNode<T> child : Node.Children) {
+        stack.add(child);
+      }
+    }
+
+  }
+
+  public List<SimpleTreeNode<T>> GetAllNodes() {
+    List<SimpleTreeNode<T>> res = new ArrayList<SimpleTreeNode<T>>();
+    // ваш код выдачи всех узлов дерева в определённом порядке
+    if (Root == null) {
+      return res;
+    }
+
+    Stack<SimpleTreeNode<T>> stack = new Stack<SimpleTreeNode<T>>();
+    stack.add(Root);
+
+    while (!stack.empty()) {
+      SimpleTreeNode<T> Node = stack.pop();
+      res.add(Node);
+
+      if (Node.Children == null) {
+        continue;
+      }
+
+      for (SimpleTreeNode<T> child : Node.Children) {
+        stack.add(child);
+      }
+    }
+
+    return res;
+  }
+
+  public List<SimpleTreeNode<T>> FindNodesByValue(T val) {
+    // ваш код поиска узлов по значению
+    List<SimpleTreeNode<T>> res = new ArrayList<SimpleTreeNode<T>>();
+    // ваш код выдачи всех узлов дерева в определённом порядке
+    if (Root == null) {
+      return res;
+    }
+
+    Stack<SimpleTreeNode<T>> stack = new Stack<SimpleTreeNode<T>>();
+    stack.add(Root);
+
+    while (!stack.empty()) {
+      SimpleTreeNode<T> Node = stack.pop();
+
+      if (Node.NodeValue.equals(val)) {
+        res.add(Node);
+      }
+
+      if (Node.Children == null) {
+        continue;
+      }
+
+      for (SimpleTreeNode<T> child : Node.Children) {
+        stack.add(child);
+      }
+    }
+
+    return res;
+  }
+
+  public void MoveNode(SimpleTreeNode<T> OriginalNode, SimpleTreeNode<T> NewParent) {
+    if (Root == null) {
+      return;
+    }
+
+    Optional<SimpleTreeNode<T>> from = recSearch(OriginalNode);
+    Optional<SimpleTreeNode<T>> to = recSearch(NewParent);
+
+    if (from.isPresent() && to.isPresent()) {
+      OriginalNode.Parent.Children.remove(from.get());
+      NewParent.AddChild(from.get());
+    }
+
+  }
+
+  public int Count() {
+    if (Root == null) {
+      return 0;
+    }
+    int count = 0;
+
+    Stack<SimpleTreeNode<T>> stack = new Stack<SimpleTreeNode<T>>();
+    stack.add(Root);
+
+    while (!stack.empty()) {
+      SimpleTreeNode<T> Node = stack.pop();
+      count++;
+
+      if (Node.Children == null) {
+        continue;
+      }
+
+      for (SimpleTreeNode<T> child : Node.Children) {
+        stack.add(child);
+      }
+    }
+
+    return count;
+  }
+
+  public int LeafCount() {
+    if (Root == null) {
+      return 0;
+    }
+    int leafs = 0;
+
+    Stack<SimpleTreeNode<T>> stack = new Stack<SimpleTreeNode<T>>();
+    stack.add(Root);
+
+    while (!stack.empty()) {
+      SimpleTreeNode<T> Node = stack.pop();
+
+      if (Node.Children == null) {
+        leafs++;
+        continue;
+      }
+
+      for (SimpleTreeNode<T> child : Node.Children) {
+        stack.add(child);
+      }
+    }
+
+    return leafs;
+  }
+
+  private Optional<SimpleTreeNode<T>> recSearch(SimpleTreeNode<T> Desire) {
+    if (Root == null) {
+      return Optional.empty();
+    }
+
+    Stack<SimpleTreeNode<T>> stack = new Stack<SimpleTreeNode<T>>();
+    stack.add(Root);
+
+    while (!stack.empty()) {
+      SimpleTreeNode<T> Node = stack.pop();
+      if (Node.equals(Desire)) {
+        return Optional.of(Node);
+      }
+
+      if (Node.Children == null) {
+        continue;
+      }
+
+      for (SimpleTreeNode<T> child : Node.Children) {
+        stack.add(child);
+      }
+    }
+    return Optional.empty();
+  }
+}
---
+++ java/tree/SimpleTreeNodeTests.java
@@ -0,0 +1,357 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class SimpleTreeNodeTests {
+    
+    @Test
+    public void Create() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        assertTrue(sut.Count() == 0);
+   }
+
+   @Test
+   public void AddEmpty() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        SimpleTreeNode<Integer> root = new SimpleTreeNode<Integer>(0, null);
+        sut.AddChild(null, root);
+
+        assertTrue(sut.Count() == 1);
+        assertTrue(sut.Root == root);
+    }
+
+    @Test
+    public void AddChildrenToExistingRoot() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        SimpleTreeNode<Integer> root = new SimpleTreeNode<Integer>(0, null);
+        sut.AddChild(null, root);
+
+        List<SimpleTreeNode<Integer>> nodes = new ArrayList<SimpleTreeNode<Integer>>();
+        for (int i = 1; i < 11; i++) {
+            nodes.add(new SimpleTreeNode<Integer>(i, null));
+        }
+        
+
+        for (SimpleTreeNode<Integer> node : nodes) {
+            sut.AddChild(root, node);
+        }
+
+        assertTrue(sut.Count() == 11);
+    }
+
+
+    @Test
+    public void AddChildrenToChild() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        SimpleTreeNode<Integer> root = new SimpleTreeNode<Integer>(0, null);
+        sut.AddChild(null, root);
+        sut.AddChild(root, new SimpleTreeNode<Integer>(1, null));
+
+        List<SimpleTreeNode<Integer>> nodes = new ArrayList<SimpleTreeNode<Integer>>();
+        for (int i = 2; i < 12; i++) {
+            nodes.add(new SimpleTreeNode<Integer>(i-1, null));
+        }
+        
+
+        for (SimpleTreeNode<Integer> node : nodes) {
+            sut.AddChild(root, node);
+        }
+
+        assertTrue(sut.Count() == 12);
+    }
+
+    @Test
+    public void AddChildToNotExisting() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        SimpleTreeNode<Integer> root = new SimpleTreeNode<Integer>(0, null);
+        sut.AddChild(null, root);
+
+
+        SimpleTreeNode<Integer> notExisting = new SimpleTreeNode<Integer>(-1, null);
+        List<SimpleTreeNode<Integer>> nodes = new ArrayList<SimpleTreeNode<Integer>>();
+        for (int i = 2; i < 12; i++) {
+            nodes.add(new SimpleTreeNode<Integer>(-1, null));
+        }
+        
+
+        for (SimpleTreeNode<Integer> node : nodes) {
+            sut.AddChild(notExisting, node);
+        }
+
+        assertTrue(sut.Count() == 1);
+    }
+
+    @Test
+    public void AddChildToNull() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        SimpleTreeNode<Integer> root = new SimpleTreeNode<Integer>(0, null);
+        sut.AddChild(null, root);
+
+        List<SimpleTreeNode<Integer>> nodes = new ArrayList<SimpleTreeNode<Integer>>();
+        for (int i = 2; i < 12; i++) {
+            nodes.add(new SimpleTreeNode<Integer>(-1, null));
+        }
+        
+
+        for (SimpleTreeNode<Integer> node : nodes) {
+            sut.AddChild(null, node);
+        }
+
+        assertTrue(sut.Count() == 1);
+    }
+
+    @Test
+    public void GetNodesFromEmpty() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        List<SimpleTreeNode<Integer>> res = sut.GetAllNodes();
+        
+
+        assertTrue(res.size() == 0);
+    }
+
+    @Test
+    public void GetAllNodesFromFilled() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        SimpleTreeNode<Integer> root = new SimpleTreeNode<Integer>(0, null);
+        sut.AddChild(null, root);
+        sut.AddChild(root, new SimpleTreeNode<Integer>(1, null));
+
+        List<SimpleTreeNode<Integer>> nodes = new ArrayList<SimpleTreeNode<Integer>>();
+        for (int i = 2; i < 12; i++) {
+            nodes.add(new SimpleTreeNode<Integer>(i-1, null));
+        }
+        
+
+        for (SimpleTreeNode<Integer> node : nodes) {
+            sut.AddChild(root, node);
+        }
+
+        List<SimpleTreeNode<Integer>> res = sut.GetAllNodes();
+
+        assertTrue(res.size() == sut.Count() && res.size() > 6);
+    }
+
+     @Test
+    public void FindAllNodesFromEmpty() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        List<SimpleTreeNode<Integer>> res = sut.FindNodesByValue(-1);
+        
+
+        assertTrue(res.size() == 0);
+    }
+
+    @Test
+    public void FindAllNodesFromFilled() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        SimpleTreeNode<Integer> root = new SimpleTreeNode<Integer>(0, null);
+        sut.AddChild(null, root);
+        sut.AddChild(root, new SimpleTreeNode<Integer>(1, null));
+
+        List<SimpleTreeNode<Integer>> nodes = new ArrayList<SimpleTreeNode<Integer>>();
+        for (int i = 2; i < 12; i++) {
+            nodes.add(new SimpleTreeNode<Integer>(i, null));
+        }
+        
+
+        for (SimpleTreeNode<Integer> node : nodes) {
+            sut.AddChild(root, node);
+        }
+
+        for ( SimpleTreeNode<Integer> node : nodes) {
+            List<SimpleTreeNode<Integer>> res = sut.FindNodesByValue(node.NodeValue);
+            assertTrue(res.size() == 1);
+        }
+    }
+
+    @Test
+    public void FindAllNodesFromFilledNotExisted() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        SimpleTreeNode<Integer> root = new SimpleTreeNode<Integer>(0, null);
+        sut.AddChild(null, root);
+        sut.AddChild(root, new SimpleTreeNode<Integer>(1, null));
+
+        List<SimpleTreeNode<Integer>> nodes = new ArrayList<SimpleTreeNode<Integer>>();
+        for (int i = 2; i < 12; i++) {
+            nodes.add(new SimpleTreeNode<Integer>(i, null));
+        }
+        
+
+        for (SimpleTreeNode<Integer> node : nodes) {
+            sut.AddChild(root, node);
+        }
+
+        for ( int i = 0; i < 12; i++) {
+            List<SimpleTreeNode<Integer>> res = sut.FindNodesByValue(-1);
+            assertTrue(res.size() == 0);
+        }
+    }
+
+    @Test
+    public void LeafsCountInEmpty() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+       
+        assertTrue(sut.LeafCount() == 0);
+    }
+
+    @Test
+    public void LeafsCountInSingle() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        SimpleTreeNode<Integer> root = new SimpleTreeNode<Integer>(0, null);
+        sut.AddChild(null, root);
+        
+        assertTrue(sut.LeafCount() == 0);
+    }
+
+    @Test
+    public void LeafsCountInFilled_1() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        SimpleTreeNode<Integer> root = new SimpleTreeNode<Integer>(0, null);
+        sut.AddChild(null, root);
+
+        List<SimpleTreeNode<Integer>> nodes = new ArrayList<SimpleTreeNode<Integer>>();
+        for (int i = 1; i < 11; i++) {
+            nodes.add(new SimpleTreeNode<Integer>(i, null));
+        } 
+
+        for (SimpleTreeNode<Integer> node : nodes) {
+            sut.AddChild(root, node);
+        }
+
+        assertTrue(sut.LeafCount() == 10);
+    }
+
+    @Test
+    public void DeleteFromEmpty() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        SimpleTreeNode<Integer> delete = new SimpleTreeNode<Integer>(1, null);
+        
+        int before = sut.Count();
+        sut.DeleteNode(delete);
+        int after = sut.Count();
+
+        assertTrue(before == after);
+    }
+
+
+    @Test
+    public void DeleteRoot() {
+         SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        SimpleTreeNode<Integer> root = new SimpleTreeNode<Integer>(0, null);
+        sut.AddChild(null, root);
+        sut.AddChild(root, new SimpleTreeNode<Integer>(1, null));
+
+        List<SimpleTreeNode<Integer>> nodes = new ArrayList<SimpleTreeNode<Integer>>();
+        for (int i = 2; i < 12; i++) {
+            nodes.add(new SimpleTreeNode<Integer>(i, null));
+        }
+
+        int before = sut.Count();
+        sut.DeleteNode(root);
+        int after = sut.Count();
+
+        assertTrue(before-after == before);
+    }
+
+    @Test
+    public void DeleteExisting () {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        SimpleTreeNode<Integer> root = new SimpleTreeNode<Integer>(0, null);
+        SimpleTreeNode<Integer> l1 = new SimpleTreeNode<Integer>(1, null);
+        SimpleTreeNode<Integer> l2 = new SimpleTreeNode<Integer>(2, null);
+        SimpleTreeNode<Integer> r2 = new SimpleTreeNode<Integer>(3, null);
+     
+        sut.AddChild(null, root);
+        sut.AddChild(root, l1);
+        sut.AddChild(l1, l2);
+        sut.AddChild(l1, r2);
+     
+
+        sut.DeleteNode(l1);
+        int after = sut.Count();
+
+        assertTrue(after == 1);
+    }
+
+     @Test
+    public void DeleteNotExisting() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        SimpleTreeNode<Integer> root = new SimpleTreeNode<Integer>(0, null);
+        SimpleTreeNode<Integer> l1 = new SimpleTreeNode<Integer>(1, null);
+        SimpleTreeNode<Integer> l2 = new SimpleTreeNode<Integer>(2, null);
+        SimpleTreeNode<Integer> r2 = new SimpleTreeNode<Integer>(3, null);
+        SimpleTreeNode<Integer> notExists = new SimpleTreeNode<Integer>(3, null);
+     
+        sut.AddChild(null, root);
+        sut.AddChild(root, l1);
+        sut.AddChild(l1, l2);
+        sut.AddChild(l1, r2);
+     
+
+        int before = sut.Count();
+        sut.DeleteNode(notExists);
+        int after = sut.Count();
+
+        assertTrue(after == before);
+    }
+
+    @Test
+    public void MoveInEmpty() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+
+        SimpleTreeNode<Integer> from = new SimpleTreeNode<Integer>(1, null);
+        SimpleTreeNode<Integer> to = new SimpleTreeNode<Integer>(1, null);
+
+        sut.MoveNode(from, to);
+
+        assertTrue(from.Parent == null);
+    }
+
+    @Test
+    public void MoveInFilled1() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        SimpleTreeNode<Integer> root = new SimpleTreeNode<Integer>(0, null);
+        SimpleTreeNode<Integer> l1 = new SimpleTreeNode<Integer>(1, null);
+        SimpleTreeNode<Integer> l2 = new SimpleTreeNode<Integer>(2, null);
+        SimpleTreeNode<Integer> r2 = new SimpleTreeNode<Integer>(3, null);
+
+        sut.AddChild(null, root);
+        sut.AddChild(root, l1);
+        sut.AddChild(l1, l2);
+        sut.AddChild(l1, r2);
+
+       
+        int before = root.Children.size();
+        sut.MoveNode(r2, root);
+        int after = root.Children.size();
+
+        assertTrue(after-1 == before);
+        assertTrue(r2.Parent.equals(root));
+    }
+
+     @Test
+    public void MoveToNewParentWithNullChildren() {
+        SimpleTree<Integer> sut = new SimpleTree<Integer>(null);
+        SimpleTreeNode<Integer> root = new SimpleTreeNode<Integer>(0, null);
+        SimpleTreeNode<Integer> l1 = new SimpleTreeNode<Integer>(1, null);
+        SimpleTreeNode<Integer> r1 = new SimpleTreeNode<Integer>(1, null);
+        SimpleTreeNode<Integer> ll2 = new SimpleTreeNode<Integer>(2, null);
+        SimpleTreeNode<Integer> lr2 = new SimpleTreeNode<Integer>(3, null);
+
+        sut.AddChild(null, root);
+        sut.AddChild(root, l1);
+        sut.AddChild(root, r1);
+        sut.AddChild(l1, ll2);
+        sut.AddChild(l1, lr2);
+
+       
+        sut.MoveNode(l1, r1);
+        int after = r1.Children.size();
+
+        assertTrue(after == 1);
+        assertTrue(l1.Parent.equals(r1));
+        assertFalse(root.Children.contains(l1));
+    }
+}
